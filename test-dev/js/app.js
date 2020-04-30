1b5058592c22efc45b6508cd7399a2bc4748ea71
bellice <bellice@tuta.io>
2020-05-01 00:39:55 +0200

    import init.json

--- test-dev/js/app.js
+++ test-dev/js/app.js
@@ -3,34 +3,22 @@ Vue.component("cohesion-factor", {
   props: ["name"],
 });
 
-new Vue({
-  el: "#app",
-  data: {
-    items: [
-      {
-        id: "000",
-        name: "Solidarité",
-      },
-      {
-        id: "001",
-        name: "Qualité de vie",
-      },
-      {
-        id: "010",
-        name: "Transition socio-environnementale",
-      },
-      {
-        id: "011",
-        name: "Capacitation des territoires",
-      },
-      {
-        id: "100",
-        name: "Capital social des territoires",
-      },
-      {
-        id: "101",
-        name: "Coopération entre les territoires",
-      },
-    ],
-  },
-});
+const request = new Request("assets/init.json");
+
+const getData = async (url) => {
+  const response = await fetch(url);
+  if (response.ok) {
+    const result = await response.json();
+    return result;
+  }
+  throw new Error(response.status);
+};
+
+getData(request).then((data) => {
+  new Vue({
+    el: "#app",
+    data: {
+      items: data.cohesionFactor,
+    },
+  });
+}); //request data
