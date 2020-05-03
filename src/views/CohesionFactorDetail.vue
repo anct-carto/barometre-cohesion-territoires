93d84361d30b2a297e2f49648c60eb4496f5ab72
bellice <bellice@tuta.io>
2020-05-03 19:55:24 +0200

    add view for architecture cohesionFactor

---
+++ src/views/CohesionFactorDetail.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div
+      id="h-statistical-indicator"
+      :class="`cohesion-factor-${cohesionFactor.id}-color`"
+    >
+      <div>{{ cohesionFactor.name }}</div>
+    </div>
+    <TheStatisticalIndicatorCard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TheStatisticalIndicatorCard from "@/components/TheStatisticalIndicatorCard.vue";
+//init.json
+import initJson from "@/assets/init.json";
+
+export default {
+  name: "CohesionFactorDetail",
+  components: {
+    TheStatisticalIndicatorCard,
+  },
+  data() {
+    return {};
+  },
+  props: {
+    cohesionFactorSlug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    cohesionFactor() {
+      return initJson.cohesionFactors.find(
+        (cohesionFactor) => cohesionFactor.slug === this.cohesionFactorSlug
+      );
+    },
+  },
+};
+</script>
---
+++ src/views/StatisticalIndicatorDetail.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div
+      id="h-statistical-indicator"
+      :class="`cohesion-factor-${cohesionFactor.id}-color`"
+    >
+      <div>{{ cohesionFactor.name }}</div>
+    </div>
+    Je suis directement sur la page de l'indicateur
+    <div>{{ cohesionFactor.name }}</div>
+  </div>
+</template>
+
+<script>
+//init.json
+import initJson from "@/assets/init.json";
+
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    cohesionFactorSlug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    cohesionFactor() {
+      return initJson.cohesionFactors.find(
+        (cohesionFactor) => cohesionFactor.slug === this.cohesionFactorSlug
+      );
+    },
+  },
+};
+</script>
