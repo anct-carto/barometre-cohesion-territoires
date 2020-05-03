94ed6c6050a573caa3bf6c7996f26b7542fa81e6
bellice <bellice@tuta.io>
2020-05-03 19:52:47 +0200

    add route cohesionFactor and statisticalIndicator

--- src/router/index.js
+++ src/router/index.js
@@ -8,21 +8,42 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    props: true,
   },
   {
     path: "/about",
     name: "About",
+    props: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/facteur-cohesion/:cohesionFactorSlug",
+    name: "CohesionFactorDetail",
+    props: true,
+    component: () =>
+      import(
+        /* webpackChunkName: "CohesionFactorDetail" */ "../views/CohesionFactorDetail"
+      ),
+  },
+  {
+    path: "/facteur-cohesion/:cohesionFactorSlug/:statisticalIndicatorId",
+    name: "StatisticalIndicatorDetail",
+    props: true,
+    component: () =>
+      import(
+        /* webpackChunkName: "StatisticalIndicatorDetail" */ "../views/StatisticalIndicatorDetail"
+      ),
+  },
 ];
 
 const router = new VueRouter({
-  routes
+  mode: "history",
+  routes,
 });
 
 export default router;
